491b871d697f482d207e009e7cb65d07adfc3546
Abdalla Yusuf <me@a135y.com>
2022-10-04 16:05:15 +0100

    Update PokemonCard

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { Navigation } from './components/Navigation';
-import { PokemonCard } from './components/PokemonCard';
+import PokemonCard from './components/PokemonCard';
+
 
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
+
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(async () => {
+    const data = await fetch(pokeApi)
+    const json = await data.json()
+    console.log(json)
+    const final = json.results;
+    setData(final);
+    console.log(data);
+  }, [])
+
 
   return (
     <div data-testid="app">
       <Navigation />
-
       <h1>Pokemon should appear here</h1>
-      <PokemonCard />
+
+      {data.map(d =>
+        <PokemonCard name={d.name} url={d.url}></PokemonCard>)}
+
+      {/* <PokemonCard name={"charmander"} url={"https://pokeapi.co/api/v2/pokemon/4"}></PokemonCard> */}
     </div>
   );
+
 }
 
 export { App };
